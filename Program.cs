3b983a8eb336fff8b3af0a5f1b90f3ce22101e41
Philippe Parisot <pparisot3@gmail.com>
2022-04-30 21:11:58 -0400

    1st try merge sort

--- Program.cs
+++ Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Hello
+
+internal static class MergeSortClass
+{
+    static void MergeSort(int[] array)
+    {
+        int[] helper = new int[array.Length];
+        MergeSort(array, helper, 0, array.Length - 1);
+    }
+
+    private static void MergeSort(int[] array, int[] helper, int low, int high)
+    {
+        if (low < high)
+        {
+            int middle = (low + high) / 2;
+            MergeSort(array, helper, low, middle);
+            MergeSort(array, helper, middle + 1, high);
+            Merge();
+        }
+    }
+
+    private void Merge(int[] array, int[] helper, int low, int middle, int high)
+    {
+        for (int i = low; i <= high; i++)
+        {
+            helper[i] = array[i];
+        }
+
+        int helperLeft = low;
+        int helperRight = middle + 1;
+        int current = low;
+
+        while (helperLeft <= middle && helperRight <= high)
+        {
+            if (helper[helperLeft] <= helper[helperRight])
+            {
+                array[current] = helper[helperRight];
+                helperLeft++;
+            }
+            else
+            {
+                array[current] = helper[helperRight];
+                helperRight++;
+            }
+            current++;
+        }
+
+        int remaining = middle - helperLeft;
+        for (int i = 0; i <= remaining; i++)
+        {
+            array[current + i] = helper[helperLeft + i];
+        }
+    }
+}
